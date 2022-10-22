8a79c43ab02db2900f94ae94b559bd1f80dca3ad
hoangtuan123 <rammatuan@gmail.com>
2022-10-22 23:42:26 +0700

    Update code

---
+++ pymongo/mongoDB_v1/owid-covid-data.py
@@ -0,0 +1,21 @@
+import pymongo
+import json
+from pymongo import MongoClient
+client = pymongo.MongoClient(
+    "mongodb+srv://admin:tuan1234567890@cluster0.xnznafc.mongodb.net/?retryWrites=true&w=majority")
+db = client["test"]
+Collection = db["name"]
+
+# Loading or Opening the json file
+with open('C:\\Users\\ramma\\Desktop\\Covid-19-news\\data-covid19\\public\\data-covid\\json\\owid-covid-data.json') as file:
+    file_data = json.load(file)
+
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+if isinstance(file_data, list):
+    Collection.insert_many(file_data)
+    
+else:
+    Collection.insert_many(file_data)
+client.close()
