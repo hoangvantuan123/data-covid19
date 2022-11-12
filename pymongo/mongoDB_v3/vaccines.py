272c0332095b011d25fdb061cb77a28d927c1c7d
hoangtuan123 <rammatuan@gmail.com>
2022-11-12 23:40:13 +0700

    Python

---
+++ pymongo/mongoDB_v3/vaccines.py
@@ -0,0 +1,20 @@
+import pymongo
+import json
+from pymongo import MongoClient
+client =pymongo.MongoClient("mongodb+srv://admin:tuan1234567890@cluster0.rkkoenm.mongodb.net/?retryWrites=true&w=majority")
+db = client["vaccines"]
+Collection =db["vaccinesCountry"]
+
+ 
+# Loading or Opening the json file
+with open('C:\\Users\\ramma\\Desktop\\Covid-19-news\\data-covid19\\public\\vaccines\\json\\vaccine-data.json') as file:
+    file_data = json.load(file)
+     
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+if isinstance(file_data, list):
+    Collection.insert_many(file_data) 
+else:
+    Collection.insert_one(file_data)
+client.close()
